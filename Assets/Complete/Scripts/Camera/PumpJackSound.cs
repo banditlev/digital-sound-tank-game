623edd8036ef437957e5b67d44523864b3597379
banditlev <sditlev@hotmail.com>
2016-01-03 13:24:48 +0100

    pumper sound added

---
+++ Assets/Complete/Scripts/Camera/PumpJackSound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PumpJackSound : MonoBehaviour {
+
+	public AudioClip m_pumpJackSound;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("PlayPumpJackSound", 0, 28);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void PlayPumpJackSound(){
+		AudioSource.PlayClipAtPoint(m_pumpJackSound, GameObject.FindGameObjectWithTag("pump1").transform.position, 10.0F);
+
+	}
+}
